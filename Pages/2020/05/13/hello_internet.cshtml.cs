1adb6a7d91700d552aa002b1f8641aefc6f1f41c
Keenan Irvin Benning <keenan@catalystdigitalventures.com>
2020-05-11 01:54:06 -0400

    added blog and razor runtime nuget pack

---
+++ Pages/2020/05/13/hello_internet.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cdv_razor.Pages._2020._05._13
+{
+    public class hello_internetModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Pages/blog.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cdv_razor.Pages
+{
+    public class blogModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
