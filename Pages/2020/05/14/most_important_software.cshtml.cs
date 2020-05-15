7c10d44b29cd30994af3510481b59c77e270e54e
Keenan Irvin Benning <keenan@catalystdigitalventures.com>
2020-05-15 01:49:14 -0400

    added new blog post...most important piece of software today.

---
+++ Pages/2020/05/14/most_important_software.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cdv_razor.Pages._2020._05._14
+{
+    public class most_important_softwareModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
