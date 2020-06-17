7aeac54d11b21eac7568cf8e6e7b3e3ea4327f02
Keenan Irvin Benning <keenan@catalystdigitalventures.com>
2020-06-17 01:25:49 -0400

    verifying file integrity blog post

---
+++ Pages/2020/06/16/verify_program_hash.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cdv_razor.Pages._2020._06._16
+{
+    public class verify_program_hashModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
