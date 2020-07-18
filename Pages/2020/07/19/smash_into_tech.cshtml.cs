40aaee10af0e10834ed924632b61ed6655a97d91
Keenan Irvin-Benning <keenan@catalystdigitalventures.com>
2020-07-18 13:06:54 -0400

    smash into tech blog post wip

---
+++ Pages/2020/07/19/smash_into_tech.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cdv_razor.Pages._2020._07._19
+{
+    public class smash_into_techModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
