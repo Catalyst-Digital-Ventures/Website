e0af25e253a48b3af29d9384275e3414e19e08f9
Keenan Irvin-Benning <kirvinb@pm.me>
2021-02-09 22:17:54 -0500

    init rough draft of is it too late for bitcoin

---
+++ Pages/2021/02/10/too-late-bitcoin.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cdv_razor.Pages
+{
+    public class too_late_bitcoin_Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
